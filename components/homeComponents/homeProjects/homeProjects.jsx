8d59879793c5de221dd9e5aefef928c63b49032b
jecordjotse <jeromecordjotse@gmail.com>
2022-04-22 21:20:35 +0000

    update pictures

--- components/homeComponents/homeProjects/homeProjects.jsx
+++ components/homeComponents/homeProjects/homeProjects.jsx
@@ -14,12 +14,10 @@ const HomeProjects = () => {
                 <ProjectItem count={3} name={"Appointment App API"} link={'https://github.com/jecordjotse/appointment-app-api'}/>
                 <ProjectItem count={3} name={"NLP"} image={'/nlp.png'}link={'https://jeromecordjotse.shinyapps.io/pletelyApp/'}/>
             </ProjectLineItem>
-            <ProjectLineItem itemCnt={2}>
-                <ProjectItem count={2} name={"Appointment App"} link={'https://github.com/jecordjotse/appointment-app'}/>
-                <ProjectItem count={2} name={"Edanra Blog"} image={'/blog.png'} link={'https://blog.edanra.com'}/>
-            </ProjectLineItem>
-            <ProjectLineItem itemCnt={1}>
-                <ProjectItem count={1} name={"BMI Calculator"} image={'/bmi_calculator.png'} link={'https://github.com/jecordjotse/shinyApp'}/>
+            <ProjectLineItem itemCnt={3}>
+                <ProjectItem count={3} name={"Appointment App"} link={'https://github.com/jecordjotse/appointment-app'}/>
+                <ProjectItem count={3} name={"Edanra Blog"} image={'/blog.png'} link={'https://blog.edanra.com'}/>
+                <ProjectItem count={3} name={"BMI Calculator"} image={'/bmi_calculator.png'} link={'https://github.com/jecordjotse/shinyApp'}/>
             </ProjectLineItem>
           </>
       </PrjectsWrap>
