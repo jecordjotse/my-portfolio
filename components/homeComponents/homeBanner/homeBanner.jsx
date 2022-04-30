137991a86aef52269cb9462f38499a0a8b75b4b6
jecordjotse <jeromecordjotse@gmail.com>
2022-04-30 16:52:10 +0000

    update

--- components/homeComponents/homeBanner/homeBanner.jsx
+++ components/homeComponents/homeBanner/homeBanner.jsx
@@ -7,81 +7,12 @@ import Button from '@mui/material/Button';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import { useTheme } from '@mui/material/styles';
-import { Gear } from '../../svg/gears';
-const images = [
-  {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      '/banner1.webp',
-  },
-  {
-    label: 'Bird',
-    imgPath:
-    '/banner2.webp',
-  },
-  {
-    label: 'Bali, Indonesia',
-    imgPath:
-    '/banner3.webp',
-  },
-];
+import { Gear } from '../../svg/gears_v2';
 
 const HomeBanner = () => {
-  const theme = useTheme
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
-
-  // useEffect(()=>{
-  //     setTimeout(() => {
-  //       setActiveStep(activeStep!==2? activeStep+1 : 0)
-  //     }, 10000);
-  // })
   return (
-    <BannerWrap bannerImage={images[0].imgPath}>
+    <BannerWrap >
       <Gear />
-        {/* <Carousel /> */}
-        {/* <MobileStepper
-      style={{position:"absolute", width:"100%", bottom:"0", background:"rgba(255, 255, 255, 0.5)"}}
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      /> */}
     </BannerWrap>
   )
 }
--- components/homeComponents/homeSkills/homeSkills.jsx
+++ components/homeComponents/homeSkills/homeSkills.jsx
@@ -8,9 +8,23 @@ const HoemSkills = () => {
     <SkillsWrap>
       <TitleWrap><h2>My Skills</h2></TitleWrap>
       <Skills>
-        <p>I am an electrical engineer and a software developer withover 4 years experience as a fullstack developer. I love MCU and I play the guitar. I also participate in kaggle competitions (very rarely). My dream is to build a fully automated house with an AI house keeper</p>
-        <SkillItem skill="Software Development" percent="97" />
-        <SkillItem skill="Electrical Circuit Design" percent="89" />
+        <p>I am an electrical engineer and a software developer with over 4 years experience as a fullstack developer. I love MCU and I play the guitar. I also participate in kaggle competitions (very rarely). My dream is to build a fully automated house with an AI house keeper</p>
+        <div style={{display:"flex"}}>
+        <ul style={{flex:"1 1 auto"}}>
+          <li>Nodejs</li>
+          <li>PHP/MySQL</li>
+          <li>Google Cloud (GAE, GCE, Marketplace, functions)</li>
+          <li>AWS Elastic Beanstalk</li>
+          <li>NoSQL (MongoDB, firestore)</li>
+        </ul>
+        <ul style={{flex:"1 1 auto"}}>
+          <li>HTML5/CSS</li>
+          <li>JS</li>
+          <li>React/Nextjs</li>
+          <li>Angularjs</li>
+          <li>Wordpress</li>
+        </ul>
+        </div>
       </Skills>
     </SkillsWrap>
   </SkillsMainWrap>
